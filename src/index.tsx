3294c547a40bf5c09391219416bdbff52677285b
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-01-27 00:35:56 +0100

    create index.css in styles folder in src, add import of index.css in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
