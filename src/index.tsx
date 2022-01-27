7b5ae2c9d90cf01befa6f563ee904c5633c51e5a
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-01-27 17:30:55 +0100

    add AppRouter inside index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import { AppRouter } from './router/AppRouter';
 import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <AppRouter/>
   </React.StrictMode>,
   document.getElementById('root')
 );
