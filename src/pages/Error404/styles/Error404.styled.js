bb31d223a99a1b1aab5a561d8bd5f1a053593759
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-01-28 10:10:20 +0100

    add Error404.styled.js in folder Error404/styles

---
+++ src/pages/Error404/styles/Error404.styled.js
@@ -0,0 +1,7 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { PrimaryButtonStyle } from '../../../styles/PrimaryButtonStyle'
+
+export const LinkStyled = styled(Link)`
+    ${PrimaryButtonStyle}
+`
