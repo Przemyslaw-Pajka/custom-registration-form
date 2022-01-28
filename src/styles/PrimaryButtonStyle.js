81bd5dd0fe162cd00b5f153fa8144f051c8921ee
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-01-28 10:06:03 +0100

    add PrimaryButtonStyle.js in styles

---
+++ src/styles/PrimaryButtonStyle.js
@@ -0,0 +1,14 @@
+
+import { css } from 'styled-components';
+
+export const PrimaryButtonStyle = css`
+    background: var(--color-topaz);
+    border: none;
+    color: #fff;
+    text-decoration: none;
+    padding: 17px 15px;
+    border-radius: 10px;
+    display: block;
+    width: max-content;
+    height: max-content;
+`
