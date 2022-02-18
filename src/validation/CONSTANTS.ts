098cfa3ad04770aeb2e64568e70a918434a35aa0
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-02-18 16:51:43 +0100

    add possibility enter special character to password input

--- src/validation/CONSTANTS.ts
+++ src/validation/CONSTANTS.ts
@@ -20,7 +20,7 @@ export const regExp:IRegExp = {
     phone: new RegExp(/^(?:(?:(?:(?:\+|00)\d{2})?[ -]?(?:(?:\(0?\d{2}\))|(?:0?\d{2})))?[ -]?(?:\d{3}[- ]?\d{2}[- ]?\d{2}|\d{2}[- ]?\d{2}[- ]?\d{3}|\d{7})|(?:(?:(?:\+|00)\d{2})?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}))$/),
     payerNumber: new RegExp(`^[0-9]{${PAYER_NUMBER_LENGTH}}$`),
     oneDigit: new RegExp(`(?=.*?[0-9])`),
-    amountChar: new RegExp(`([A-Za-z0-9]{${MIN_PASSWORD_LENGTH},}$)`),
+    amountChar: new RegExp(`([A-Za-z0-9!@#$%^&*]{${MIN_PASSWORD_LENGTH},}$)`),
     uppLowLetter: new RegExp(`(?=.*[a-z])(?=.*[A-Z])`),
     get password() {
         return new RegExp(`${this.oneDigit.source}${this.uppLowLetter.source}${this.amountChar.source}`)
