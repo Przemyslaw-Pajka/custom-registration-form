a4421e2eaf8ba902817f1a4aa2e8a4cc332cbd3b
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-02-28 01:28:26 +0100

    add files (sendForm.ts, checkIsUserCreated.ts, CONSTANTS.TS)

---
+++ src/utils/CONSTANTS.ts
@@ -0,0 +1,1 @@
+export const URL_TO_SEND_DATA = 'http://127.0.0.23:5000/send'
---
+++ src/utils/sendForm.ts
@@ -0,0 +1,24 @@
+import { DataFromUser, SetState } from "../types/types";
+import { checkIsUserCreated } from "./checkIsUserCreated";
+import { URL_TO_SEND_DATA } from "./CONSTANTS";
+
+export const sendForm = (dataFromUser:DataFromUser, setIsLoading:SetState, setErrors:SetState, setIsUserRegistrationConfirmed:SetState) => {
+    setIsLoading(true);
+
+    fetch(URL_TO_SEND_DATA, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataFromUser)
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            const response = JSON.parse(JSON.stringify(data))
+            
+            checkIsUserCreated(response, setIsUserRegistrationConfirmed, setIsLoading, setErrors)
+        })
+        .catch(err => console.log(err))
+}
