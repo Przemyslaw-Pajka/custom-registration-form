aca8041bb6a0efc06b9ab7a2e7237cf70f258180
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-03-01 12:22:38 +0100

    Add onClick event im SubmitButton.tsx. Refactored SubmitButtonStyled.js

--- src/components/SubmitButton/SubmitButton.tsx
+++ src/components/SubmitButton/SubmitButton.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import { SubmitButtonStyled } from "./styles/SubmitButton.styled"
 
-export const SubmitButton:React.FC<React.HTMLAttributes<HTMLDivElement>> = (props) => (
-    <SubmitButtonStyled className={props.className}>
+export const SubmitButton:React.FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
+    return(
+    <SubmitButtonStyled className={props.className} onClick={props.onClick}>
         {props.children}
     </SubmitButtonStyled>
-)
+)}
