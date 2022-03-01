c95a787c788114d788027583777fd4213e4773d7
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-03-01 12:19:42 +0100

    Add new props, possibility of sending Form in RegistrationForm.tsx

--- src/components/RegistrationForm/Container/RegistrationForm.tsx
+++ src/components/RegistrationForm/Container/RegistrationForm.tsx
@@ -1,6 +1,8 @@
 import { Formik } from "formik";
-import React from "react";
+import React, { useContext } from "react";
+import { StoreContext } from "../../../store/Store";
 import { FormValues } from '../../../types/types';
+import { sendForm } from "../../../utils/sendForm";
 import { registrationValidationSchema } from '../../../validation/registrationValidationSchema';
 import { DefaultInputsForm } from "../Fields/DefaultInputsForm/DefaultInputsForm";
 import { RegistrationFormStyled } from "./styles/RegistrationForm.styled.js";
@@ -12,7 +14,15 @@ interface PropsValues{
   handleChange?: () => {}
 }
 
-export const RegistrationForm:React.FC = React.memo(() => {
+export const RegistrationForm:React.FC = React.memo((props) => {
+  const {
+    errors,
+    setErrors,
+    isLoading,
+    setIsLoading,
+    setIsUserRegistrationConfirmed
+  } = useContext(StoreContext)
+
   const initialValues:FormValues = {
     email:'',
     password: '',
@@ -25,17 +35,36 @@ export const RegistrationForm:React.FC = React.memo(() => {
   }
   return (
     <Formik
-    initialValues={initialValues}
-    validationSchema={registrationValidationSchema}
+      initialValues={initialValues}
+      validationSchema={registrationValidationSchema}
       onSubmit={(values) => {
-        console.log("SUBMITTED",values)
-      }}>
+        const dataFromUser = {
+          email: values.email,
+          password: values.password,
+          payerNumber: values.payerNumber,
+          pesel: values.pesel,
+          phone: values.phone
+        }
+        sendForm (
+          dataFromUser,
+          setIsLoading,
+          setErrors,
+          setIsUserRegistrationConfirmed
+        )
+      }}
+      >
       {(props:PropsValues) => (
         <RegistrationFormStyled
+          id="formRegistration"
           autocomplete="off"
           onReset={props.handleReset}
           onSubmit={props.handleSubmit}>
-          <DefaultInputsForm values={props.values}/>
+          <DefaultInputsForm 
+            values={props.values} 
+            errors={errors}
+            setErrors={setErrors} 
+            isLoading={isLoading}
+          />
         </RegistrationFormStyled>
       )}
     </Formik>
