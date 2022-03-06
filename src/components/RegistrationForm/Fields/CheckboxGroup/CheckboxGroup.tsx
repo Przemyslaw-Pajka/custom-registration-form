842ef776675982e287500668b548b39ab494a303
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2022-03-06 18:16:14 +0100

    Foreign customers version v1.0.0

--- src/components/RegistrationForm/Fields/CheckboxGroup/CheckboxGroup.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { CheckboxGroupStyled } from "./styles/CheckboxGroup.styled"
-
-export const CheckboxGroup: React.FC = React.memo((props) => (
-    <CheckboxGroupStyled>
-        {props.children}
-    </CheckboxGroupStyled>
-))
--- src/components/SubmitButton/SubmitButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { SubmitButtonStyled } from "./styles/SubmitButton.styled"
-
-export const SubmitButton:React.FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
-    return(
-    <SubmitButtonStyled className={props.className} onClick={props.onClick}>
-        {props.children}
-    </SubmitButtonStyled>
-)}
